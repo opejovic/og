f7ed414e3b6fcaefc673337f3d5b0b4952e3f98f
Ognjen <opejovic@gmail.com>
2021-03-17 23:17:30 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex justify-between pt-4 2xl:pt-20">
+  <div class="flex justify-between pt-4 lg:pt-12 xl:pt-28">
     <!-- Left side -->
-    <div class="w-3/5 pt-10 border-l border-dashed pl-10 border-gray-400">
+    <div class="w-3/5 pt-10 border-l border-dashed pl-6 border-gray-400">
       <div
         class="font-light pb-10 text-justify"
         style="font-size: 26px; line-height: 118.6%; letter-spacing: -0.04em"
@@ -28,7 +28,7 @@
             <li>Prototype</li>
           </ul>
         </div>
-        <div class="py-6 border-l border-gray-400 border-dashed pl-10">
+        <div class="py-6 border-l border-gray-400 border-dashed pl-6">
           <h3
             class="pb-6"
             style="font-weight: bold; font-size: 28px; line-height: 108.1%"
@@ -43,7 +43,7 @@
             <li>Illustration</li>
           </ul>
         </div>
-        <div class="py-6 border-l border-gray-400 border-dashed pl-10">
+        <div class="py-6 border-l border-gray-400 border-dashed pl-6">
           <h3
             class="pb-6"
             style="font-weight: bold; font-size: 28px; line-height: 108.1%"
@@ -62,30 +62,20 @@
     </div>
 
     <!-- Right side -->
-    <div class="flex items-center">
-      <svg
-        width="246"
-        height="150"
-        viewBox="0 0 246 150"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M0 110L0 80H30V110H0Z" fill="#007DA4" />
-        <path d="M30 110V80H60V110H30Z" fill="currentColor" />
-        <path d="M156 110V80H186V110H156Z" fill="currentColor" />
-        <path d="M216 110V80H246V110H216Z" fill="#F1B901" />
-        <path d="M100 110V80H130V110H100Z" fill="#D80630" />
-        <path d="M100 150V120H130V150H100Z" fill="#9A968A" />
-        <path d="M100 30V0L130 0V30H100Z" fill="#9A968A" />
-      </svg>
+    <div class="flex items-center text-dfc-dark dark:text-dfc-light">
+      <JosefMullerBoxesBigger />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import JosefMullerBoxesBigger from "@/components/JosefMullerBoxesBigger.vue";
 
 export default defineComponent({
+  components: {
+    JosefMullerBoxesBigger
+  },
   setup() {
     return {};
   },

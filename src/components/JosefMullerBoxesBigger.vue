c898f09be09b145a27ba83eedabdafd97bc5e16e
Ognjen <opejovic@gmail.com>
2021-03-17 23:17:26 +0100

    Create JosefMullerBoxesBigger.vue

---
+++ src/components/JosefMullerBoxesBigger.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    width="246"
+    height="150"
+    viewBox="0 0 246 150"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0 110L0 80H30V110H0Z" fill="#007DA4" />
+    <path d="M30 110V80H60V110H30Z" fill="currentColor" />
+    <path d="M156 110V80H186V110H156Z" fill="currentColor" />
+    <path d="M216 110V80H246V110H216Z" fill="#F1B901" />
+    <path d="M100 110V80H130V110H100Z" fill="#D80630" />
+    <path d="M100 150V120H130V150H100Z" fill="#9A968A" />
+    <path d="M100 30V0L130 0V30H100Z" fill="#9A968A" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>

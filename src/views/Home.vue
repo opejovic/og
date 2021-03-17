4860afbb9cbdeab1b95fa8d84d65c1c7af47cb72
Ognjen <opejovic@gmail.com>
2021-03-17 23:17:32 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,52 @@
 <template>
-  <div class="flex justify-between items-center pt-4 2xl:pt-20">
-    <div
-      class="w-4/5 text-landing 2xl:text-9xl"
-      style="
-        font-style: normal;
-        font-weight: 600;
-        line-height: 108.1%;
-        letter-spacing: -0.07em;
-      "
-    >
-      <p>Hello!</p>
-      I’m Og, a digital products maker.
-      <p>No bullshit.</p>
-
-      <p
-        class="font-light relative"
+  <div>
+    <div class="flex justify-between items-center pt-4 lg:pt-12 2xl:pt-28">
+      <div
+        class="w-3/4 text-landing text-7xl"
         style="
-          font-size: 24px;
-          line-height: 120%;
-          letter-spacing: -0.06em !important;
+          font-style: normal;
+          font-weight: 600;
+          line-height: 108.1%;
+          letter-spacing: -0.07em;
         "
       >
-        <!-- You get complete digital product, crafted with bleeding-edge
+        <p>Hello!</p>
+        I’m Og, a digital products maker.
+        <p>No bullshit.</p>
+
+        <p
+          class="font-light relative"
+          style="
+            font-size: 24px;
+            line-height: 120%;
+            letter-spacing: -0.06em !important;
+          "
+        >
+          <!-- You get complete digital product, crafted with bleeding-edge
         technologies, driven by data and research for added value. -->
-        I can help you build bespoke high-quality web applications from scratch,
-        using bleeding-edge technologies.
-      </p>
+          I can help you build bespoke high-quality web applications from
+          scratch, using bleeding-edge technologies.
+        </p>
+      </div>
+      <div>
+        <SocialMediaLinks />
+      </div>
     </div>
-    <div>
-      <SocialMediaLinks />
+
+    <!-- Curly line -->
+    <div class="pt-8 2xl:pt-16">
+      <img
+        class="hidden lg:block absolute w-full left-0"
+        src="../assets/svgs/home-line.svg"
+        alt="curly-line"
+      />
     </div>
-  </div>
+    <!-- End curly line -->
 
-  <!-- Curly line -->
-  <div class="pt-8 2xl:pt-16">
-    <img
-      class="hidden lg:block absolute w-full left-0"
-      src="../assets/svgs/home-line.svg"
-      alt="curly-line"
-    />
-  </div>
-  <!-- End curly line -->
-
-  <div class="mt-24 flex justify-between items-center">
-    <JosefMullerBoxes />
-    <a href="#" target="_blank" rel="noopener noreferrer">hello@ognjen.dev</a>
+    <div class="mt-24 flex justify-between items-center">
+      <JosefMullerBoxes />
+      <a href="#" target="_blank" rel="noopener noreferrer">hello@ognjen.dev</a>
+    </div>
   </div>
 </template>
 
